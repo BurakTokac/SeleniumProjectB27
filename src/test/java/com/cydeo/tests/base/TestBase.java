082089386b4549247126f7a6ba07884f11d13343
Aysun120 <aysundamar114@gmail.com>
2022-12-18 22:21:23 -0600

    TestBase class updated

--- src/test/java/com/cydeo/tests/base/TestBase.java
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -1,10 +1,13 @@
 package com.cydeo.tests.base;
 
+import com.cydeo.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
 
 // This class is the parent of all Test classes
 // You will update this class based on what you are keep writing in your most test classes
@@ -15,9 +18,9 @@ public abstract class TestBase {
 
     @BeforeMethod
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
